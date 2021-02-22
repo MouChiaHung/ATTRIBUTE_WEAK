e3167b53363ac5618fcc7813ebdfb35576d79a18
Amo <ifeelgooder@gmail.com>
2021-02-22 17:34:19 +0800

    Update board.h

--- main/inc/board.h
+++ main/inc/board.h
@@ -1,7 +1,7 @@
 #ifndef _BOARD_H_
 #define _BOARD_H_
 
-#define STARTUP_HANDLER startup_handler
+#define STARTUP_HANDLER startup_handler //to export symbol
 
 __attribute__((weak)) void foo();
 void print_startup_handler();
